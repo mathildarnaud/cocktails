9ad53c19998aae25bd9873d32efab1d3afc47d31
Mathilde Arnaud <mathildarnaud0598@gmail.com>
2023-01-11 15:11:05 +0100

    fixed responsive maybe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def default_url_options
-    { host: ENV["www.dreamcocktail.com"] || "localhost:3000" }
-  end
+  # def default_url_options
+  #   { host: ENV["www.dreamcocktail.com"] || "localhost:3000" }
+  # end
 end
