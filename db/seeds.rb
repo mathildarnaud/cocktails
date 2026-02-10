d5042502721954372aa13a634ce13d37124ec3dc
Mathilde Arnaud <111744564+mathildarnaud@users.noreply.github.com>
2026-02-10 14:37:50 -0400

    Refactor seed file for cocktails and alcools
    Removed unused imports and streamlined the seed process. Created specific cocktails directly instead of dynamically importing them.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,75 +1,24 @@
-require "json"
-require "open-uri"
-
-puts "Nettoyage de la base de données..."
+puts "Nettoyage..."
 Cocktail.destroy_all
 Alcool.destroy_all
 
 puts "Création des alcools..."
-# On stocke les alcools dans un hash pour les retrouver facilement
-dynamic_alcools = {}
-alcools_to_create = [
-  {name: "Gin", alt: 'Gin'},
-  {name: "Campari", alt: 'Campari'},
-  {name: "Rhum", alt: 'Rum'},
-  {name: "Tequila", alt: 'Tequila'},
-  {name: "Vodka", alt: 'Vodka'},
-  {name: "Whisky", alt: 'Whiskey'},
-  {name: "Scotch", alt: 'Scotch'},
-  {name: "Cognac", alt: 'Cognac'},
-  {name: "Cachaca", alt: 'Cachaca'}
-]
+vodka = Alcool.create!(name: "Vodka", alt: "vodka")
+rhum = Alcool.create!(name: "Rhum", alt: "rhum")
 
-alcools_to_create.each do |data|
-  dynamic_alcools[data[:name]] = Alcool.create!(name: data[:name], alt: data[:alt])
-end
+puts "Création des cocktails liés..."
+Cocktail.create!(
+  name: "Moscow Mule",
+  instruction: "Mélanger vodka et ginger beer.",
+  image: "https://www.thecocktaildb.com/images/media/drink/3pylqc1504370288.jpg",
+  alcool: vodka
+)
 
-puts "--- #{Alcool.count} alcools créés ---"
+Cocktail.create!(
+  name: "Mojito",
+  instruction: "Rhum, menthe, citron vert.",
+  image: "https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg",
+  alcool: rhum
+)
 
-# Méthode générique pour importer des cocktails et les lier à un alcool
-def import_cocktails_for(alcool_obj)
-  query_name = alcool_obj.alt || alcool_obj.name
-  puts "Importation des cocktails pour : #{alcool_obj.name}..."
-  
-  begin
-    # 1. On récupère la liste des cocktails pour cet ingrédient
-    list_url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{query_name.downcase}"
-    list_data = JSON.parse(URI.open(list_url).read)
-
-    if list_data['drinks']
-      # On limite à 10 cocktails par alcool pour ne pas surcharger le seed
-      list_data['drinks'].take(10).each do |item|
-        # 2. Pour chaque cocktail, on récupère les détails (instructions, etc.)
-        name_encoded = URI.encode_www_form_component(item["strDrink"])
-        detail_url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{name_encoded}"
-        detail_data = JSON.parse(URI.open(detail_url).read)
-
-        if detail_data['drinks']
-          c = detail_data['drinks'].first
-          Cocktail.create!(
-            name: c["strDrink"],
-            instruction: c["strInstructions"],
-            image: c["strDrinkThumb"],
-            # LIAISON : on associe le cocktail à l'objet alcool passé en paramètre
-            alcool: alcool_obj, 
-            ingredient1: c["strIngredient1"],
-            ingredient2: c["strIngredient2"],
-            ingredient3: c["strIngredient3"],
-            measure1: c["strMeasure1"],
-            measure2: c["strMeasure2"],
-            measure3: c["strMeasure3"]
-          )
-        end
-      end
-    end
-  rescue => e
-    puts "Erreur sur l'alcool #{alcool_obj.name} : #{e.message}"
-  end
-end
-
-# Lancement de l'import pour chaque alcool créé
-dynamic_alcools.each do |_name, alcool_obj|
-  import_cocktails_for(alcool_obj)
-end
-
-puts "--- SEED TERMINÉ : #{Cocktail.count} cocktails créés ---"
+puts "Terminé !"
